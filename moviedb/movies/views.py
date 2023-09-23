52f392430f1a0fbfad90400df94f38ce8f00a97a
sruthijaganmohan <sruthi12jagan@gmail.com>
2023-09-23 18:50:34 +0530

    details div

--- moviedb/movies/views.py
+++ moviedb/movies/views.py
@@ -7,14 +7,15 @@ def ask_omdb(searchItem):
     omdb.set_default('apikey', '134c4e2a')
     movies = omdb.search(searchItem)
     if movies:
-        movie_info = ""
+        ans = ""
         for movie in movies:
             title = movie['title']
             year = movie['year']
             poster_url = movie['poster']
-            movie_info += f"Title: {title} Year: {year}<br>"
-            movie_info += f'<img src="{poster_url}" alt="{title} Poster"><br>'
-        return movie_info
+            ans += f"Title: {title} Year: {year}<br>"
+            ans += f'<img src="{poster_url}" alt="{title} Poster"><br>'
+            ans += "ducks"
+        return list(ans.split("ducks"))[:-1]
     else:
         return "No movies found"
 
